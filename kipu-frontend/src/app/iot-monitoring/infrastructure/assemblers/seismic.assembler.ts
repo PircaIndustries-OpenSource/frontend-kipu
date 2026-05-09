cfbfd7035331043e765c8d3785fb30531122accd
Marce <marsredflag@gmail.com>
2026-05-09 12:02:11 -0500

    feat(iot-monitoring): added seismic assembler class.

---
+++ kipu-frontend/src/app/iot-monitoring/infrastructure/assemblers/seismic.assembler.ts
@@ -0,0 +1,24 @@
+import { SeismicResource, SeismicResponse } from '../models/seismic.response';
+import { SeismicEntity } from '../../domain/seismic.entity';
+
+export class SeismicAssembler {
+  static toEntityFromResource(seismicResource: SeismicResource): SeismicEntity {
+    const seismicEntity = new SeismicEntity();
+    seismicEntity.id = seismicResource.id;
+    seismicEntity.projectId = seismicResource.projectId;
+    seismicEntity.unit = seismicResource.unit;
+    seismicEntity.lastLecture = seismicResource.lastLecture;
+    seismicEntity.limit = seismicResource.limit;
+    seismicEntity.location = seismicResource.location;
+    seismicEntity.timeLecture = seismicResource.timeLecture;
+
+    const states: Record<number, string> = { 1: 'NORMAL', 2: 'RISK' };
+    seismicEntity.state = states[seismicResource.state];
+
+    return seismicEntity;
+  }
+
+  static toEntitiesFromResponse(response: SeismicResponse): SeismicEntity[] {
+    return response.map((resource) => this.toEntityFromResource(resource));
+  }
+}
